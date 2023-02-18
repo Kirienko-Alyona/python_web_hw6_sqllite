69f37723b930e2d00e87a73ea57065c4d1da807e
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-02-18 21:32:14 +0200

    changed query_12

--- query_12.py
+++ query_12.py
@@ -8,31 +8,32 @@ fake = Faker('uk-UA')
 
 #Оценки студентов в определенной группе по определенному предмету на последнем занятии.
 
-queryold= """
-SELECT s.fullname, g.name, d.name, gr.grade AS grades_students
+query = """
+SELECT s.fullname AS student, g.name AS group_, d.name AS discipline, gr.grade AS grade, gr.date_of AS date_last_lesson
 FROM grades gr
 JOIN disciplines d ON d.id = gr.discipline_id
 JOIN students s ON s.id = gr.student_id
 JOIN groups_st g ON g.id = s.group_id
 WHERE g.id = 3 AND gr.date_of IN (
     SELECT gr.date_of 
-    from grades as gr
+    FROM grades as gr
     JOIN students s ON s.id = gr.student_id
     JOIN groups_st g ON g.id = s.group_id
-    where g.id = 3 and discipline_id = 2
-    order by gr.date_of desc 
-    limit 1)
-ORDER BY grades_students;
+    WHERE g.id = 3 AND discipline_id = 2
+    ORDER BY gr.date_of desc 
+    LIMIT 1);
 """
-query= """
-
-SELECT s.fullname, g.name, d.name, gr.grade AS grades_students, gr.date_of
+#Оценки студентов в определенной группе на последнем занятии (по определенному предмету - который не указывается).
+query_plus= """
+SELECT s.fullname AS student, g.name AS group_, d.name AS discipline, gr.grade AS grade, gr.date_of AS date_last_lesson
 FROM grades gr
 JOIN disciplines d ON d.id = gr.discipline_id
 JOIN students s ON s.id = gr.student_id
 JOIN groups_st g ON g.id = s.group_id
-WHERE g.id = 3 and d.id=2 
-ORDER BY gr.date_of DESC;
+WHERE g.id = 2 AND gr.date_of IN (
+    SELECT MAX(gr.date_of)
+    FROM grades gr
+    )    
 """
 
 
@@ -41,5 +42,8 @@ if __name__ == '__main__':
     with connection() as conn:
         c = conn.cursor()
         c.execute(query)
+        pprint.pprint(c.fetchall())
+        print()
+        c.execute(query_plus)
         pprint.pprint(c.fetchall())
         c.close()
